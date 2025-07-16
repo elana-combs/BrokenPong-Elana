deca617e3dbecc955061fdbefd4c7e0a7ad695ff
Elana Combs <elana.combs@g.austincc.edu>
2025-07-15 20:18:33 -0500

    Renamed several things in Paddle.cs - Renamed s to speed. - Renamed m to moveDirection. - Renamed d to boundary. - Renamed PlayerType to currentPlayer. - Renamed ia to input.

--- Assets/Scripting/Paddle.cs
+++ Assets/Scripting/Paddle.cs
@@ -9,45 +9,45 @@ public enum PlayerType
 
 public class Paddle : MonoBehaviour
 {
-    public PlayerType playerType;
-    public float s = 10f;
-    public float d = 4f;
+    public PlayerType currentPlayer;
+    public float speed = 10f;
+    public float boundary = 4f;
 
-    [SerializeField] private InputAction ia;
-    private float m;
+    [SerializeField] private InputAction input;
+    private float moveDirection;
 
     void Awake()
     {
-        ia.performed += OnMovePerformed;
-        ia.canceled += OnMoveCanceled;
+        input.performed += OnMovePerformed;
+        input.canceled += OnMoveCanceled;
     }
 
     void OnEnable()
     {
-        ia.Enable();
+        input.Enable();
     }
     
     void OnDisable()
     {
-        ia.Disable();
+        input.Disable();
     }
 
     private void OnMovePerformed(InputAction.CallbackContext context)
     {
-        m = context.ReadValue<float> ();
+        moveDirection = context.ReadValue<float> ();
     }
 
     private void OnMoveCanceled(InputAction.CallbackContext context)
     {
-        m = 0;
+        moveDirection = 0;
     }
 
     void Update()
     {
-        float movement = m * s * Time.deltaTime;
+        float movement = moveDirection * speed * Time.deltaTime;
         transform.Translate(0f, movement, 0f);
 
-        float clampedY = Mathf.Clamp(transform.position.y, -d, d);
+        float clampedY = Mathf.Clamp(transform.position.y, -boundary, boundary);
         transform.position = new Vector3(transform.position.x, clampedY, 0f);
     }
 }
