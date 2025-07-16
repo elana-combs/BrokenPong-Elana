00ee2198d330dd6bd784d5919cf690d48286c147
Elana Combs <elana.combs@g.austincc.edu>
2025-07-15 21:15:38 -0500

    Renamed x to speed. Created RandomizeRange function for copy+pasted code.

--- Assets/Scripting/Ball.cs
+++ Assets/Scripting/Ball.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-    [SerializeField] private float x = 8f;
+    [SerializeField] private float speed = 8f;
     [SerializeField] private List<string> tags;
     [SerializeField] private string otherTag;
     private Vector2 position;
@@ -21,15 +21,23 @@ public class Ball : MonoBehaviour
 
     void Update()
     {
-        transform.Translate(position * x * Time.deltaTime);
+        transform.Translate(position * speed * Time.deltaTime);
     }
 
     //Resets the ball position to the center.
     private void ResetBall()
     {
         transform.position = Vector2.zero;
-        position = new Vector2(Random.Range(-movementRange, movementRange), Random.Range(-movementRange, movementRange)).normalized;
+        position = new Vector2(RandomizeRange(), RandomizeRange()).normalized;
     }
+
+    private float RandomizeRange()
+    {
+        float range = Random.Range(-movementRange, movementRange);
+        return range;
+    }
+ 
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag(tags[0]))
