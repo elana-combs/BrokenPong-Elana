3b77f005cfd21c9868b6d046882a2e32c67234e5
Elana Combs <elana.combs@g.austincc.edu>
2025-07-17 12:54:49 -0500

    Added Audio Source to the Ball Inspector. + Attached audio to Clip 1, Clip 2, and Clip 3. + Added audio triggers to the OnTriggerEnter2D if statements. + Renamed the audio clip variables to match their respective clips.

--- Assets/Scripting/Ball.cs
+++ Assets/Scripting/Ball.cs
@@ -18,14 +18,15 @@ public class Ball : MonoBehaviour
     private Vector2 direction;
 
     [SerializeField] private AudioSource audioSource;
-    [SerializeField] private AudioClip clip1;
-    [SerializeField] private AudioClip clip2;
-    [SerializeField] private AudioClip clip3;
+    [SerializeField] private AudioClip scoreSound;
+    [SerializeField] private AudioClip wallBounceSound;
+    [SerializeField] private AudioClip playerHitSound;
 
     private float movementRange = 1f; //range is -1, 1 -- this is to fill a variable in ranges below.
     void Start()
     {
         ResetBall();
+        audioSource = GetComponent<AudioSource>();
     }
 
     void Update()
@@ -49,17 +50,29 @@ public class Ball : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
+
+        //When the ball collides with the ScoreWall, the scoreSound will play, the ball will reset, and the score will increase for the respective player.
         if (other.CompareTag(collisionTags[(int)CollisionTag.ScoreWall]))
         {
+            audioSource.clip = scoreSound;
+            audioSource.Play();
             ResetBall();
             GameManager.IncrementScore(other.GetComponent<ScoreWall>().scoringPlayer);
         }
+
+        //When the ball collides with the BounceWall, the wallBounceSound will play, and the ball will bounce off the wall.
         else if (other.CompareTag(collisionTags[(int) CollisionTag.BounceWall]))
         {
+            audioSource.clip = wallBounceSound;
+            audioSource.Play();
             direction.y = -direction.y;
         }
+        
+        //When the ball collides with the Player, the playerHitSound will play, the direction of the ball will change, and the speed will slightly increase.
         else if (other.CompareTag(collisionTags[(int)CollisionTag.Player]))
         {
+            audioSource.clip = playerHitSound;
+            audioSource.Play();
             direction.x = -direction.x;
             direction.y = transform.position.y - other.transform.position.y;
             direction = direction.normalized;
